6027037867e2672779821f185ddd6e81290cfb92
dunghoangdacminh <105062613+dunghoangdacminh@users.noreply.github.com>
2024-07-01 19:50:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
+import signal
 import socket
 from server import server
 from client import client
 from utils import generate_password
 
+def signal_handler(sig, frame):
+    print("\nTerminating the program...")
+    exit(0)
+
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
     print("Welcome to remote desktop software")
     print("---------")
     print("Choose an option:")
     print("1. Remote a computer")
     print("2. Generate the IP and Password to others.")
+    print("Press Control + C to terminate.")
     print("---------")
     choice = int(input("Your choice: "))
     print("---------")
 
-
     if choice == 1:
         ip = input("IP address: ")
-        try:
-            port = int(input("Port: "))            
-            password = input("Password: ")
-            client.start_client(ip, port, password)
-        except:
-            print("Invalid input!")
+        port = int(input("Port: "))
+        password = input("Password: ")
+        client.start_client(ip, port, password)
     elif choice == 2:
         ip = socket.gethostbyname(socket.gethostname())
         port = 9999
